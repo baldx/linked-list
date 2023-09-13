9c9c38e33a8c58c0a54acd1854c479ec5884674e
baldx <mr.ahmad.m2006@gmail.com>
2023-09-13 15:15:43 +0200

    create node & nodeList class

---
+++ node.js
@@ -0,0 +1,15 @@
+class node {
+    constructor(value = null, next = null) {
+        return value, next;
+    }
+}
+
+class nodeList {
+    constructor (list = []) {
+        return list;
+    }
+}
+
+
+console.log(new nodeList)
+
